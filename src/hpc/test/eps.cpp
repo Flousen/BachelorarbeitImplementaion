0e7c8f44252b0e6023aea0c53b4305487e9edc32
flousen <florian.kroetz@uni-ulm.de>
2018-06-04 20:55:36 +0200

    clean up

--- src/hpc/test/eps.cpp
+++ src/hpc/test/eps.cpp
@@ -10,7 +10,8 @@
 #include <hpc/matvec/test/walltime.hpp>
 #include <hpc/matvec/test/diagdom.hpp>
 
-#include <hpc/mklblas/trmv.hpp>
+#include <hpc/mklblas.hpp>
+
 
 // from intel mkl
 #include <mkl_types.h>
@@ -18,26 +19,12 @@
 #define MAX_M 5 
 #define MAX_N 5
 
+
 int
 main()
 {
     using namespace hpc::matvec;
-
-    GeMatrix<double> A(MAX_M, MAX_N);
-    DenseVector<double> x(A.numCols());
-
-    test::rand(A);
-    
-    copy(A.row(0,0), x);
-
-    auto B = A.view(UpLo::Upper);
-
-    fmt::printf("B = \n"); print(B, "%9.4f");
-    fmt::printf("x = \n"); print(x, "%9.4f");
-    
-    hpc::mklblas::mv(1.0, B, x);    
-    
-    fmt::printf("x = \n"); print(x, "%9.4f");
-
+    printf("eps = %e\n",std::numeric_limits<double>::epsilon());
+    fmt::printf("eps = %lf \n",std::numeric_limits<double>::epsilon()); 
 
 }
